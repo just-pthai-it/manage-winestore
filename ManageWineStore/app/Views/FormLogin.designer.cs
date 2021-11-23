d477a31f7106bb1822fa24ed97649863c57b26f9
innkPaker <59672248+innkPaker@users.noreply.github.com>
2021-11-23 16:04:58 +0700

    update FormLogin, add resource file.

--- ManageWineStore/app/Views/FormLogin.designer.cs
+++ ManageWineStore/app/Views/FormLogin.designer.cs
@@ -29,6 +29,7 @@ namespace Views
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
             this.lbl_Welcome = new System.Windows.Forms.Label();
             this.ExitBt = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
@@ -39,14 +40,17 @@ namespace Views
             this.txtUsername = new System.Windows.Forms.TextBox();
             this.confirmBt = new System.Windows.Forms.Button();
             this.lbl_Credit = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lbl_Welcome
             // 
             this.lbl_Welcome.AutoSize = true;
             this.lbl_Welcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Welcome.Location = new System.Drawing.Point(96, 40);
+            this.lbl_Welcome.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lbl_Welcome.Location = new System.Drawing.Point(409, 31);
             this.lbl_Welcome.Name = "lbl_Welcome";
             this.lbl_Welcome.Size = new System.Drawing.Size(120, 25);
             this.lbl_Welcome.TabIndex = 10;
@@ -54,12 +58,13 @@ namespace Views
             // 
             // ExitBt
             // 
-            this.ExitBt.Location = new System.Drawing.Point(171, 232);
+            this.ExitBt.BackColor = System.Drawing.Color.Beige;
+            this.ExitBt.Location = new System.Drawing.Point(486, 225);
             this.ExitBt.Name = "ExitBt";
             this.ExitBt.Size = new System.Drawing.Size(87, 29);
             this.ExitBt.TabIndex = 8;
-            this.ExitBt.Text = "Exit";
-            this.ExitBt.UseVisualStyleBackColor = true;
+            this.ExitBt.Text = "Thoát";
+            this.ExitBt.UseVisualStyleBackColor = false;
             // 
             // panel1
             // 
@@ -69,7 +74,7 @@ namespace Views
             this.panel1.Controls.Add(this.lbl_Username);
             this.panel1.Controls.Add(this.txtPassword);
             this.panel1.Controls.Add(this.txtUsername);
-            this.panel1.Location = new System.Drawing.Point(26, 83);
+            this.panel1.Location = new System.Drawing.Point(339, 74);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(259, 122);
             this.panel1.TabIndex = 9;
@@ -78,34 +83,35 @@ namespace Views
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(46, 97);
+            this.label1.Location = new System.Drawing.Point(20, 96);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(168, 13);
+            this.label1.Size = new System.Drawing.Size(219, 13);
             this.label1.TabIndex = 8;
-            this.label1.Text = "Undefined username or password!";
+            this.label1.Text = "Tên đăng nhập hoặc mật khẩu không đúng!";
             // 
             // lbl_Password
             // 
             this.lbl_Password.AutoSize = true;
-            this.lbl_Password.Location = new System.Drawing.Point(21, 64);
+            this.lbl_Password.Location = new System.Drawing.Point(3, 64);
             this.lbl_Password.Name = "lbl_Password";
-            this.lbl_Password.Size = new System.Drawing.Size(56, 13);
+            this.lbl_Password.Size = new System.Drawing.Size(55, 13);
             this.lbl_Password.TabIndex = 7;
-            this.lbl_Password.Text = "Password:";
+            this.lbl_Password.Text = "Mật khẩu:";
             // 
             // lbl_Username
             // 
             this.lbl_Username.AutoSize = true;
-            this.lbl_Username.Location = new System.Drawing.Point(21, 27);
+            this.lbl_Username.Location = new System.Drawing.Point(3, 27);
             this.lbl_Username.Name = "lbl_Username";
-            this.lbl_Username.Size = new System.Drawing.Size(58, 13);
+            this.lbl_Username.Size = new System.Drawing.Size(84, 13);
             this.lbl_Username.TabIndex = 6;
-            this.lbl_Username.Text = "Username:";
+            this.lbl_Username.Text = "Tên đăng nhập:";
             // 
             // txtPassword
             // 
             this.txtPassword.Location = new System.Drawing.Point(92, 61);
             this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(139, 20);
             this.txtPassword.TabIndex = 2;
             // 
@@ -118,37 +124,52 @@ namespace Views
             // 
             // confirmBt
             // 
-            this.confirmBt.Location = new System.Drawing.Point(51, 232);
+            this.confirmBt.BackColor = System.Drawing.Color.Beige;
+            this.confirmBt.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.confirmBt.Location = new System.Drawing.Point(366, 225);
             this.confirmBt.Name = "confirmBt";
             this.confirmBt.Size = new System.Drawing.Size(87, 29);
             this.confirmBt.TabIndex = 7;
-            this.confirmBt.Text = "Confirm";
-            this.confirmBt.UseVisualStyleBackColor = true;
+            this.confirmBt.Text = "Đăng nhập";
+            this.confirmBt.UseVisualStyleBackColor = false;
             this.confirmBt.Click += new System.EventHandler(this.confirmBt_Click);
             // 
             // lbl_Credit
             // 
             this.lbl_Credit.AutoSize = true;
-            this.lbl_Credit.Location = new System.Drawing.Point(158, 285);
+            this.lbl_Credit.Location = new System.Drawing.Point(436, 293);
             this.lbl_Credit.Name = "lbl_Credit";
-            this.lbl_Credit.Size = new System.Drawing.Size(145, 13);
+            this.lbl_Credit.Size = new System.Drawing.Size(163, 13);
             this.lbl_Credit.TabIndex = 11;
-            this.lbl_Credit.Text = "Cre: Bach N., Hai P., Minh P.";
+            this.lbl_Credit.Text = "Cre: Bach Ng., Hai Ph., Minh Ph.";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(313, 291);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
             // 
             // FormLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(306, 307);
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(611, 315);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lbl_Credit);
             this.Controls.Add(this.lbl_Welcome);
             this.Controls.Add(this.ExitBt);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.confirmBt);
             this.Name = "FormLogin";
-            this.Text = "Login";
+            this.Text = "Đăng nhập";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -166,6 +187,7 @@ namespace Views
         private System.Windows.Forms.Button confirmBt;
         private System.Windows.Forms.Label lbl_Credit;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
